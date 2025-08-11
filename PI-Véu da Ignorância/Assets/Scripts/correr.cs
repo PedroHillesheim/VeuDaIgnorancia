64463edf73e5c5823f21eb5c105964e4f3c6ece6
victoriaLM230 <victoria.martins1@alunos.sc.senac.br>
2025-08-11 14:24:27 -0300

    correr

--- PI-Véu da Ignorância/Assets/Scripts/correr.cs
+++ PI-Véu da Ignorância/Assets/Scripts/correr.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
 
-public class correr: MonoBehaviour
+public class correr : MonoBehaviour
 {
-    public float walkSpeed = 5f;
+    private float horizontal;
     public float runSpeed = 9f;
-    private Rigidbody2D Body;
-
+    public float speed;
+    public GameObject bullet;
+    [SerializeField] private Rigidbody2D body;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Body = GetComponent<Rigidbody2D>();
-        if (Body == null)
-            Debug.LogError("Rigidbody2D não encontrado!");
+        body = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Debug.Log("Shift pressionado");
+        }
+        else
+        {
+            Debug.Log("Shift nï¿½o pressionado");
+        }
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
-        Body.linearVelocity = new Vector2(horizontal * speed, Body.linearVelocity.y);
+        horizontal = Input.GetAxis("Horizontal");
+
+        // Atualiza o campo speed com base no Shift
+        speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : speed;
+
+        // Agora usa o campo speed atualizado
+        body.linearVelocity = new Vector2(horizontal * speed, body.linearVelocity.y);
+
+        Debug.Log($"Velocidade atual: {speed}");
+
     }
 }
