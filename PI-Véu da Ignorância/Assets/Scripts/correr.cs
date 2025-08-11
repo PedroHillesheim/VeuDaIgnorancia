ea5e1c60672c9fe9f27cb17a7099d5ad573b2471
victoriaLM230 <victoria.martins1@alunos.sc.senac.br>
2025-08-11 14:38:08 -0300

    correr

--- PI-Véu da Ignorância/Assets/Scripts/correr.cs
+++ PI-Véu da Ignorância/Assets/Scripts/correr.cs
@@ -7,7 +7,7 @@ public class correr : MonoBehaviour
     public float speed;
     public GameObject bullet;
     [SerializeField] private Rigidbody2D body;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -15,22 +15,13 @@ public class correr : MonoBehaviour
         {
             Debug.Log("Shift pressionado");
         }
-        else
-        {
-            Debug.Log("Shift n�o pressionado");
-        }
-
+      
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
 
-        // Atualiza o campo speed com base no Shift
-        speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : speed;
-
-        // Agora usa o campo speed atualizado
         body.linearVelocity = new Vector2(horizontal * speed, body.linearVelocity.y);
 
         Debug.Log($"Velocidade atual: {speed}");
--- PI-Véu da Ignorância/Assets/Scripts/player.cs
+++ PI-Véu da Ignorância/Assets/Scripts/player.cs
@@ -2,22 +2,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float walkSpeed = 5f; 
+    public float runSpeed = 9f;
     private float horizontal;
-   
     public float speed;
+
     public GameObject bullet;
     [SerializeField]private Rigidbody2D body;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        if (body == null)
+        {
+            Debug.LogError("Rigidbody2D não encontrado!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        
+
+        speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
 
         body.linearVelocity = new Vector2 (horizontal * speed, body.linearVelocity.y);
 
