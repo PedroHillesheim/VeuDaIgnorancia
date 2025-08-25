46e41dadcb2d1c465c615716f58f80f3309d9bf5
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2025-08-25 14:38:01 -0300

    quitgame e arrumando

---
+++ PI-Véu da Ignorância/Assets/Pedro/Puzzlescripts/EndGAme.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EndGAme : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
