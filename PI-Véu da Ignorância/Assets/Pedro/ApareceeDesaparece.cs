1a8d0582b6374f3c18fc824ff005493cd7c1f610
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2025-09-02 15:51:04 -0300

    filler]

---
+++ PI-Véu da Ignorância/Assets/Pedro/ApareceeDesaparece.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ApareceeDesaparece : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Collider2D col;
+    private bool jaApareceu = false;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+        // Começa invisível mas com colisor SEMPRE ativo
+        spriteRenderer.enabled = false;
+    }
+
+    void OnMouseEnter()
+    {
+        if (!jaApareceu)
+        {
+            // Faz aparecer
+            spriteRenderer.enabled = true;
+            jaApareceu = true;
+            Debug.Log("Personagem apareceu!");
+            // Avisa o controlador do jogo que encontrou um erro
+            FindObjectOfType<PuzzleGameController>().CliqueEmErro();
+        }
+    }
+
+}
