57c3210168e3b6b5937ded8098ca6a85c7125ab5
Maria Eduarda Raulino Heinemann <maduzzzx@gmail.com>
2025-10-03 11:31:44 -0300

    tela creditos

---
+++ PI-Véu da Ignorância/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Dialogo : MonoBehaviour
+{
+    public TMP_Text textoDialogo;
+    public string[] frases;
+    private int indice = 0;
+    public GameObject painelDialogo;
+
+    void Start()
+    {
+        if (frases.Length > 0)
+        {
+            textoDialogo.text = frases[0];
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ProximaFrase();
+        }
+    }
+
+    void ProximaFrase()
+    {
+        indice++;
+
+        if (indice < frases.Length)
+        {
+            textoDialogo.text = frases[indice];
+        }
+        else
+        {
+
+            painelDialogo.SetActive(false);
+
+
+         
+        }
+    }
+}
