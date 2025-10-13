6b8639b08db307e8c0ebc4f8cfecce19a4f1e409
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2025-10-13 16:35:41 -0300

    ReconstruçãoComeço

---
+++ PI-Véu da Ignorância/Assets/Pedro/Puzzlescripts/Recriação da arte de mira/ReconstrucaoCodigo.cs
@@ -0,0 +1,67 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ReconstrucaoCodigo : MonoBehaviour
+{
+    public int totalItems = 3;
+    private int collectedItems = 0;
+    private GameObject currentItem = null;
+    private GameObject victoryZone = null;
+    public TMP_Text collectItemText;
+    public UnityEvent victoryPanel;
+    private bool isNearVictoryZone = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            collectItemText.text = collectedItems.ToString();
+            if (currentItem != null)
+            {
+                collectedItems++;
+                Destroy(currentItem);
+                currentItem = null;
+                Debug.Log("Item coletado! (" + collectedItems + "/" + totalItems + ")");
+            }
+
+            // Se já pegou todos os itens e está na zona de vitória
+            else if (isNearVictoryZone && collectedItems >= totalItems)
+            {
+                ShowVictory();
+            }
+        }
+    }
+
+    void ShowVictory()
+    {
+        if (victoryPanel != null)
+        {
+            victoryPanel.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            currentItem = other.gameObject;
+        }
+        else if (other.CompareTag("VictoryZone"))
+        {
+            isNearVictoryZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Item") && currentItem == other.gameObject)
+        {
+            currentItem = null;
+        }
+        else if (other.CompareTag("VictoryZone"))
+        {
+            isNearVictoryZone = false;
+            Debug.Log("Você saiu da zona de vitória.");
+        }
+    }
+}
