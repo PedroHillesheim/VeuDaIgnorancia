0f14d4695097c09d2f9ab8f1390c77d79d1d2656
victoriaLM230 <victoria.martins1@alunos.sc.senac.br>
2025-11-05 15:27:41 -0300

    ajustes do dialogo

--- PI-Véu da Ignorância/Assets/Scripts/Dialogue.cs
+++ PI-Véu da Ignorância/Assets/Scripts/Dialogue.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+<<<<<<< Updated upstream
+=======
+using Unity.VisualScripting;
+using System;
+>>>>>>> Stashed changes
 
-public class Dialogue: MonoBehaviour
+public class Dialogue : MonoBehaviour
 {
     public TMP_Text textoDialogo;
     public string[] frases;
     private int indice = 0;
     public GameObject painelDialogo;
+<<<<<<< Updated upstream
 
     void Start()
     {
         if (frases.Length > 0)
+=======
+    public AudioSource[] Fala;
+    public AudioSource Ambient;
+    public int CurrentAudio = 0;
+
+    void Start()
+    {
+        if (Ambient != null)
+        {
+            Ambient.Play();
+        }
+
+        if (frases != null && frases.Length > 0)
+>>>>>>> Stashed changes
         {
             textoDialogo.text = frases[0];
+        }
+        else
+        {
+            Debug.LogWarning("Nenhuma frase foi atribuída ao diálogo!");
         }
     }
 
@@ -29,17 +53,48 @@ public class Dialogue: MonoBehaviour
     {
         indice++;
 
+<<<<<<< Updated upstream
+=======
+        // Verifica se ainda há frases
+>>>>>>> Stashed changes
         if (indice < frases.Length)
         {
             textoDialogo.text = frases[indice];
+
+            // Só toca o áudio se existir um correspondente
+            if (indice < Fala.Length && Fala[indice] != null)
+            {
+                Fala[indice].Play();
+            }
         }
         else
         {
-
+            // Fim do diálogo
             painelDialogo.SetActive(false);
+<<<<<<< Updated upstream
 
 
          
         }
     }
 }
+=======
+            Debug.Log("Fim do diálogo!");
+        }
+    }
+
+    public void SomAtual()
+    {
+        // Garante que não vá além do número de áudios
+        if (CurrentAudio < Fala.Length && Fala[CurrentAudio] != null)
+        {
+            Fala[CurrentAudio].Play();
+            Invoke(nameof(SomAtual), Fala[CurrentAudio].clip.length);
+            CurrentAudio++;
+        }
+    }
+}
+
+
+
+>>>>>>> Stashed changes
