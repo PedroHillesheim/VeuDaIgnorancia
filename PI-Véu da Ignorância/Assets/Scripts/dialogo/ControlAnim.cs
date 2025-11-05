b770ddd2a1db95ff9ed0986ecc1c1b688ee885ea
victoriaLM230 <victoria.martins1@alunos.sc.senac.br>
2025-11-05 11:40:13 -0300

    audios

---
+++ PI-Véu da Ignorância/Assets/Scripts/dialogo/ControlAnim.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ControlAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandlerPostDialogue()
+    {
+
+    }
+}
