1e39350214eb0892ef40daba75401d72d14d8c1b
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2025-11-06 13:08:30 -0300

    Revert "ForcaAprimorado"
    This reverts commit 45c9226daeca69f9e902ce8063ae03ee6e0bbf41.

--- PI-Véu da Ignorância/Assets/Pedro/Puzzlescripts/TrocadeTela/TrocadeTela.cs
+++ PI-Véu da Ignorância/Assets/Pedro/Puzzlescripts/TrocadeTela/TrocadeTela.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class TrocadeTela : MonoBehaviour
 {
+    PuzzleGameController puzzleGameController;
+    public UnityEvent loseScreen;
+    private void Update()
+    {
+        puzzleGameController = FindFirstObjectByType(typeof(PuzzleGameController)) as PuzzleGameController;
+    }
+
     public void OutraScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
